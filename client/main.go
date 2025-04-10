42cc087b7eb7ceff45e44cdbbd5d43173a605d6a
Facundo Carusi <fcarusi@frba.utn.edu.ar>
2025-04-10 16:35:33 -0300

    loggeo la config

--- client/main.go
+++ client/main.go
@@ -13,9 +13,15 @@ func main() {
 	log.Println("Hola soy un log")
 
 	globals.ClientConfig = utils.IniciarConfiguracion("config.json")
+
 	// validar que la config este cargada correctamente
+	if globals.ClientConfig == nil {
+		log.Println("Error: La configuración no se cargó correctamente.")
+		return
+	}
 
 	// loggeamos el valor de la config
+	log.Println("Valor de la configuración:", globals.ClientConfig)
 
 	// ADVERTENCIA: Antes de continuar, tenemos que asegurarnos que el servidor esté corriendo para poder conectarnos a él
 
